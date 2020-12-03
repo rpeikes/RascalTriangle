c124c7b55771c574fadcf8bcc631332955997f4d
Ricki Peikes <rickipeikes@gmail.com>
2020-12-02 22:04:53 -0500

    extracted methods in rascal

--- src/main/java/Rascal.java
+++ src/main/java/Rascal.java
@@ -7,21 +7,33 @@ public class Rascal {
     }
 
     public int getValue(int row, int hIndex) {
-        if (hIndex == 0 || hIndex == row + 1 || row == 0) {
+        if (isOnPerimeter(row, hIndex)) {
             return 1;
-        } else if (hIndex == 1 || hIndex == row) {
+        } else if (isNextToPerimeter(row, hIndex)) {
             return row;
         } else {
-            if (hIndex > row / 2) {
-                hIndex = row - hIndex;
-            }
-            int val = row;
-            int spacer = row - 3;
-            for (int i = 2; i <= hIndex; i++) {
-                val += spacer;
-                spacer -= 2;
-            }
-            return val;
+            return traverseRowToIndex(row, hIndex);
         }
+    }
+
+    private int traverseRowToIndex(int row, int hIndex) {
+        if (hIndex > row / 2) {
+            hIndex = row - hIndex;
+        }
+        int val = row;
+        int spacer = row - 3;
+        for (int i = 2; i <= hIndex; i++) {
+            val += spacer;
+            spacer -= 2;
+        }
+        return val;
+    }
+
+    private boolean isNextToPerimeter(int row, int hIndex) {
+        return hIndex == 1 || hIndex == row;
+    }
+
+    private boolean isOnPerimeter(int row, int hIndex) {
+        return hIndex == 0 || hIndex == row + 1 || row == 0;
     }
 }
