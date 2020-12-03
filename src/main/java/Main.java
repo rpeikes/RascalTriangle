ea593f4b0f853d2d0c402f99eb163edc803bb156
Ricki Peikes <rickipeikes@gmail.com>
2020-12-02 23:50:41 -0500

    extracted methods in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Rascal rascal = new Rascal();
+
         try {
             Scanner scanner = new Scanner(new File(args[0]));
             int dataSets = scanner.nextInt();
             int[] rows = new int[dataSets];
             int[] hIndices = new int[dataSets];
-            for (int i = 0; i < dataSets; i++) {
-                scanner.nextInt();
-                rows[i] = scanner.nextInt();
-                hIndices[i] = scanner.nextInt();
-            }
-            for (int i = 0; i < rows.length; i++) {
-                System.out.println((i+1) + " " + rascal.getValue(rows[i], hIndices[i]));
-            }
+            readInput(scanner, dataSets, rows, hIndices);
+            printSolution(new Rascal(), rows, hIndices);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void readInput(Scanner scanner, int dataSets, int[] rows, int[] hIndices) {
+        for (int i = 0; i < dataSets; i++) {
+            scanner.nextInt();
+            rows[i] = scanner.nextInt();
+            hIndices[i] = scanner.nextInt();
+        }
+    }
+
+    private static void printSolution(Rascal rascal, int[] rows, int[] hIndices) {
+        for (int i = 0; i < rows.length; i++) {
+            System.out.println((i+1) + " " + rascal.getValue(rows[i], hIndices[i]));
         }
     }
 }
